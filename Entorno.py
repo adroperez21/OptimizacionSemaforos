b234aee0edb2b6509696adb98b74db3b1d9b0acb
adrianperezgarrone <adrianperez1292@gmail.com>
2017-04-24 22:47:25 -0300

    commit lunes 24

---
+++ Entorno.py
@@ -0,0 +1,28 @@
+class parametros:
+    def set_NGEN(self, valor):
+        self.NGEN = valor
+
+    def set_CXPB(self, valor):
+        self.CXPB = valor
+
+    def set_MUTPB(self, valor):
+        self.MUTPB = valor
+
+    def set_individual_min(self, valor):
+        self.individual_min = valor
+
+    def set_individual_max(self, valor):
+        self.individual_max = valor
+
+    def set_individual_lengh(self, valor):
+        self.individual_lengh = valor
+
+    def set_population(self, valor):
+        self.population = valor
+
+    def set_indbp(self, valor):
+        self.indbp = valor
+
+    def set_tournament_size(self, valor):
+        self.tournament_size = valor
+
